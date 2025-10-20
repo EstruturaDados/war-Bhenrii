47da8e262f82a485dfe7a1e656a6ff35b8f9d632
Bruno Braz <brunohenriquebraz3@gmail.com>
2025-10-20 19:46:16 -0300

    Criação do projeto WAR

--- Projeto-War.c
+++ Projeto-War.c
@@ -3,17 +3,20 @@
 #define MAX_NOME 100
 #define QTD_TERRITORIOS 5
 
-struct Territorio {
+struct Territorio
+{
     char nome[MAX_NOME];
     char cor[MAX_NOME];
     int numero;
 };
 
-int main() {
+int main()
+{
     struct Territorio territorio[QTD_TERRITORIOS];
     int totaltropa = 0;
 
-    while (totaltropa < QTD_TERRITORIOS) {
+    while (totaltropa < QTD_TERRITORIOS)
+    {
         printf("==========================================================\n");
         printf(" VAMOS CADASTRAR OS TERRITORIOS INICIAIS DO NOSSO MUNDO\n");
         printf(" --- CADASTRANDO TERRITORIO %d ---\n", totaltropa + 1);
@@ -36,7 +39,8 @@ int main() {
     printf("         MAPA DO MUNDO - ESTADO ATUAL  \n");
     printf("==============================================\n\n");
 
-    for (int i = 0; i < QTD_TERRITORIOS; i++) {
+    for (int i = 0; i < QTD_TERRITORIOS; i++)
+    {
         printf("TERRITORIO %d:\n", i + 1);
         printf(" - Nome: %s", territorio[i].nome);
         printf(" - Dominado por: %s", territorio[i].cor);
